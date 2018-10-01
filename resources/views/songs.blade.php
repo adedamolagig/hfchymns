d10588105b0854c9c50f8efce0c9f60cfbff5ec8
adedamolagig <adedamolagig@gmail.com>
2018-10-01 02:41:37 +0100

    trying to solve error of Trying to get property title of non-object

--- resources/views/songs.blade.php
+++ resources/views/songs.blade.php
@@ -2,7 +2,7 @@
 
 <div id="lyrics-container" class="text-center">  
     <div id="lyrics-header">
-        <h1>{{$song->title}}</h1>
+        <h1>{{@$song['title']}}</h1>
         <span id="lyrics-album-details">
             {{$song->artist->name}} - {{$song->album}} Album
         </span>  
@@ -14,7 +14,7 @@
         </div><!--/#lyrics-thumbnail-->
     </div><!--/#lyrics-header-->
     <div id="lyrics-content">
-        <p><strong>"{{$song->title}}"</strong></p>
+        <p><strong>"{{@$song['title']}}"</strong></p>
         <p>{!! nl2br($song->lyrics) !!}</p>
     </div><!--/#lyrics-content-->
 </div> 
